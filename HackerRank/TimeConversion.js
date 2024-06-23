0b47136be24a7898da477668325eaa2e65b07dbe
rohanchandane <rohan.chandane@gmail.com>
2024-06-23 03:29:16 +0100

    update a doc

--- HackerRank/TimeConversion.js
+++ HackerRank/TimeConversion.js
@@ -2,20 +2,20 @@
 
 function timeConversion(s) {
 
-const [hours, minutes, partialSeconds] = s.split(":");
-const amPm = partialSeconds.slice(-2);
-const seconds = partialSeconds.slice(0,2)
+	const [hours, minutes, partialSeconds] = s.split(":");
+	const amPm = partialSeconds.slice(-2);
+	const seconds = partialSeconds.slice(0,2)
 
-let parsedHours = parseInt(hours, 10);
-console.log(hours, parsedHours)
+	let parsedHours = parseInt(hours, 10);
+	console.log(hours, parsedHours)
 
-if(amPm === "PM" && parsedHours !== 12) {
-	parsedHours += 12;
-} else if (amPm === "AM" && parsedHours === 12) {
-	parsedHours = 0;
-}
+	if(amPm === "PM" && parsedHours !== 12) {
+		parsedHours += 12;
+	} else if (amPm === "AM" && parsedHours === 12) {
+		parsedHours = 0;
+	}
 
-return `${parsedHours.toString().padStart(2, '0')}:${minutes}:${seconds}`;
+	return `${parsedHours.toString().padStart(2, '0')}:${minutes}:${seconds}`;
 }
 
 
