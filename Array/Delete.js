d0828b9b3a41b79be2e416592b5d80cfda9546e9
rohanchandane <rohan.chandane@gmail.com>
2024-06-23 13:00:00 +0100

    added console.logs

--- Array/Delete.js
+++ Array/Delete.js
@@ -3,9 +3,9 @@ const numbers = [1,2,3,4,5];
 // delete from the end
 numbers.pop();
 
-console.log(numbers);
+console.log(numbers); // [ 1, 2, 3, 4 ]
 
 // delete from the start
 numbers.shift();
 
-console.log(numbers);
+console.log(numbers); // [ 2, 3, 4 ]
